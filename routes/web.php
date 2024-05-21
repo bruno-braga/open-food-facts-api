396f0a2e0dd17f2e10b8ca713f6804dbdf3d9cbc
Bruno <brunobraga@protonmail.com>
2024-05-21 00:39:12 -0300

    Add health / endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+$oneMb = 1048576;
+
+Route::get('/', function () use ($oneMb) {
+    phpinfo();
+    return response()->json([
+        'db_connection' => !is_null(DB::connection()->getPdo()) ? 'Connected' : 'Not connected',
+        'memory_usage' => (memory_get_usage(true) / $oneMb) . 'MB',
+    ]);
 });
